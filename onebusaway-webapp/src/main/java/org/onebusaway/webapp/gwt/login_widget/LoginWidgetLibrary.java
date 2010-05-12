d28c54aa6e723e222aec1eb7adc510bd4a709ff1
Brian Ferris <bdferris@google.com>
2010-05-12 03:51:57 +0000

    Bug fix for dealing with /onebusaway-webapp vs /

--- onebusaway-webapp/src/main/java/org/onebusaway/webapp/gwt/login_widget/LoginWidgetLibrary.java
+++ onebusaway-webapp/src/main/java/org/onebusaway/webapp/gwt/login_widget/LoginWidgetLibrary.java
@@ -1,6 +1,7 @@
 package org.onebusaway.webapp.gwt.login_widget;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window.Location;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class LoginWidgetLibrary implements EntryPoint {
@@ -8,8 +9,11 @@ public class LoginWidgetLibrary implements EntryPoint {
   @Override
   public void onModuleLoad() {
     
+    String path = Location.getPath();
+    String prefix = path.contains("/onebusaway-webapp") ? "/onebusaway-webapp" : "";
+    
     RootPanel panel = RootPanel.get("login_widget");
-    panel.add(new LoginWidget("/onebusaway-webapp/services/login",
-        "/onebusaway-webapp/login-handler.action"));
+    panel.add(new LoginWidget(prefix + "/services/login",
+        prefix + "/login-handler.action"));
   }
 }
