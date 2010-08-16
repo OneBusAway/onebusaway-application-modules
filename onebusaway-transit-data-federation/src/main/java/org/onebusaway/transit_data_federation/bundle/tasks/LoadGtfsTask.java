d9dc612d0dcd57afbf28bdc81657c4888d9762de
David Turner <novalis@novalis.org>
2010-08-16 23:35:48 +0000

    Actually let spring do the autowiring itself, and get the bean by name

--- onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/bundle/tasks/LoadGtfsTask.java
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/bundle/tasks/LoadGtfsTask.java
@@ -3,7 +3,6 @@ package org.onebusaway.transit_data_federation.bundle.tasks;
 import org.hibernate.SessionFactory;
 import org.onebusaway.gtfs.impl.HibernateGtfsRelationalDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 
 public class LoadGtfsTask implements Runnable {
@@ -25,8 +24,7 @@ public class LoadGtfsTask implements Runnable {
   @Override
   public void run() {
     try {
-      AutowireCapableBeanFactory factory = _applicationContext.getAutowireCapableBeanFactory();
-      HibernateGtfsRelationalDaoImpl store = factory.createBean(HibernateGtfsRelationalDaoImpl.class);
+      HibernateGtfsRelationalDaoImpl store = new HibernateGtfsRelationalDaoImpl();
       store.setSessionFactory(_sessionFactory);
       GtfsReadingSupport.readGtfsIntoStore(_applicationContext, store);
     } catch (Throwable ex) {
