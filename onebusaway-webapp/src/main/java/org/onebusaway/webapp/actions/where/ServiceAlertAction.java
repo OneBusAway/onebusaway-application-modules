94a8e7f8d01f41d3abc16c6862809a5c6e445ec2
Brian Ferris <bdferris@google.com>
2011-01-12 12:11:31 +0000

    Service Alert tweaks

--- onebusaway-webapp/src/main/java/org/onebusaway/webapp/actions/where/ServiceAlertAction.java
+++ onebusaway-webapp/src/main/java/org/onebusaway/webapp/actions/where/ServiceAlertAction.java
@@ -1,11 +1,15 @@
 package org.onebusaway.webapp.actions.where;
 
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Actions;
+import org.onebusaway.transit_data.model.service_alerts.NaturalLanguageStringBean;
 import org.onebusaway.transit_data.model.service_alerts.SituationBean;
 import org.onebusaway.transit_data.services.TransitDataService;
+import org.onebusaway.users.services.CurrentUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -16,6 +20,8 @@ public class ServiceAlertAction extends ActionSupport {
   private static final long serialVersionUID = 1L;
 
   private TransitDataService _transitDataService;
+  
+  private CurrentUserService _currentUserService;
 
   private String _id;
 
@@ -24,6 +30,11 @@ public class ServiceAlertAction extends ActionSupport {
   @Autowired
   public void setTransitDataService(TransitDataService transitDataService) {
     _transitDataService = transitDataService;
+  }
+  
+  @Autowired
+  public void setCurrentUserService(CurrentUserService currentUserService) {
+    _currentUserService = currentUserService;
   }
 
   @RequiredStringValidator(key = "requiredField")
@@ -48,6 +59,21 @@ public class ServiceAlertAction extends ActionSupport {
     _situation = _transitDataService.getServiceAlertForId(_id);
     if (_situation == null)
       throw new NoSuchElementException();
+    NaturalLanguageStringBean desc = _situation.getDescription();
+    if( desc != null && desc.getValue() != null) {
+      String value = desc.getValue();
+      Pattern p = Pattern.compile("(http://[^ ]+) ");
+      Matcher m = p.matcher(value);
+      StringBuffer sb = new StringBuffer();
+      while (m.find()) {
+          m.appendReplacement(sb, "<a href=\"" + m.group(1) + "\">" + m.group(1) + "</a> ");
+      }
+      m.appendTail(sb);
+      desc.setValue(sb.toString());
+    }
+    
+    _currentUserService.markServiceAlertAsRead(_situation.getId(), System.currentTimeMillis(), true);
+    
     return SUCCESS;
   }
 }
