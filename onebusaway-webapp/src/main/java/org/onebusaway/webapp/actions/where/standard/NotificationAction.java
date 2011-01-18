06ffebd3ef7db9d34121c1416dae8faf553f4e4d
Brian Ferris <bdferris@google.com>
2011-01-18 08:58:12 +0000

    1) Hopefully complete the migration from the GWT NotificationApplication to the jQuery-based solution. 2) Refactor the stop.jspx to use the common templates also used by arrival-and-departure-for-stop

--- onebusaway-webapp/src/main/java/org/onebusaway/webapp/actions/where/standard/NotificationAction.java
+++
@@ -1,9 +0,0 @@
-package org.onebusaway.webapp.actions.where.standard;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class NotificationAction extends ActionSupport {
-
-  private static final long serialVersionUID = 1L;
-
-}
