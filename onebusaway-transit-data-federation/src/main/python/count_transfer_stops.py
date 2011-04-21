0e439d3ba8dfa81faf591d44f405c541d94ca21b
Brian Ferris <bdferris@google.com>
2011-04-21 21:37:21 +0000

    Add transfer counters

---
+++ onebusaway-transit-data-federation/src/main/python/count_transfer_stops.py
@@ -0,0 +1,49 @@
+#!/bin/env python                                                                                                                                                                                                                 
+
+import fileinput
+import csv
+import sys
+
+def main():
+
+    isSegmentStart = newSegmentStartMapping()
+
+    fi = fileinput.FileInput(openhook=fileinput.hook_compressed)
+
+    transferCounts = {}
+
+    for line in fi:
+
+        if fi.isfirstline():
+            isSegmentStart = newSegmentStartMapping()
+
+        for row in csv.reader([line]):
+
+            if len(row) == 3:
+                isSegmentStart = newSegmentStartMapping()
+
+            index = row[0]
+            stopId = row[1]
+
+            parentIndex = '-1'
+            if len(row) == 4:
+                parentIndex = row[3]
+
+            parentWasSegmentStart = isSegmentStart[parentIndex]
+            currentIsSegmentStart = not parentWasSegmentStart
+
+            isSegmentStart[index] = currentIsSegmentStart
+
+            if stopId not in transferCounts:
+                transferCounts[stopId] = long(0);
+
+            transferCounts[stopId] += 1
+
+    for key,value in transferCounts.items():
+        print "%d\t%s" % (value,key)
+
+def newSegmentStartMapping():
+    return {'-1': True}
+
+if __name__ == '__main__':
+    main()
