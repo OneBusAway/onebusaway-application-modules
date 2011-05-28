fee9d57ea0d1c6d9370d51373f22b47ed9b9ae15
Brian Ferris <bdferris@google.com>
2011-05-28 17:52:55 +0000

    1) Another stab at getting the Selenium integration tests to work with Firefox 4 (complete upgrade to Selenium 2.0) 2) Site documentation.

--- onebusaway-federations-webapp/src/test/java/org/onebusaway/integration/federations_webapp/WebInterfaceTest.java
+++ onebusaway-federations-webapp/src/test/java/org/onebusaway/integration/federations_webapp/WebInterfaceTest.java
@@ -1,40 +1,70 @@
 package org.onebusaway.integration.federations_webapp;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.thoughtworks.selenium.DefaultSelenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebInterfaceTest {
 
-  private DefaultSelenium selenium;
+  private WebDriver driver;
 
   @Before
-  public void setUp() throws Exception {
-    int port = Integer.parseInt(System.getProperty(
-        "org.onebusaway.federations_webapp.port", "8080"));
-    System.out.println("port=" + port);
-    selenium = new DefaultSelenium("localhost", port, "*firefox", "/onebusaway-federations-webapp");
+  public void setup() {
+    driver = new FirefoxDriver();
+  }
+
+  @After
+  public void teardown() {
+    driver.close();
   }
 
   @Test
   public void testUntitled() throws Exception {
-    selenium.open("/onebusaway-federations-webapp/remoting/index.action");
-    selenium.type("url", "http://onebusaway.org/");
-    selenium.type("serviceClass", "org.onebusaway.TransitData");
-    selenium.click("submit");
-    selenium.waitForPageToLoad("30000");
+
+    int port = Integer.parseInt(System.getProperty(
+        "org.onebusaway.federations_webapp.port", "9900"));
+    System.out.println("port=" + port);
+
+    driver.get("http://localhost:" + port
+        + "/onebusaway-federations-webapp/remoting/index.action");
+
+    WebElement urlElement = driver.findElement(By.name("url"));
+    urlElement.sendKeys("http://onebusaway.org/");
+
+    WebElement serviceClassElement = driver.findElement(By.name("serviceClass"));
+    serviceClassElement.sendKeys("org.onebusaway.TransitData");
+
+    WebElement submitElement = driver.findElement(By.name("submit"));
+    submitElement.click();
+
     assertEquals("org.onebusaway.TransitData",
-        selenium.getText("//tr[2]/td[1]"));
-    assertEquals("http://onebusaway.org/", selenium.getText("//tr[2]/td[2]/a"));
-    assertEquals("true", selenium.getText("//tr[2]/td[3]"));
-    selenium.click("//tr[2]/td[5]/a");
-    selenium.waitForPageToLoad("30000");
-    assertEquals("false", selenium.getText("//tr[2]/td[3]"));
-    selenium.click("//tr[2]/td[6]/a");
-    selenium.waitForPageToLoad("30000");
-    assertFalse(selenium.isTextPresent("org.onebusaway.TransitData"));
+        driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[1]")).getText());
+    assertEquals("http://onebusaway.org/",
+        driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[2]/a")).getText());
+    assertEquals("true",
+        driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[3]")).getText());
+
+    driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[5]/a")).click();
+
+    assertEquals("false",
+        driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[3]")).getText());
+
+    driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[6]/a")).click();
+
+    try {
+      WebElement element = driver.findElement(By.xpath("//table[@id='services']/tbody/tr[2]/td[1]"));
+      System.out.println(element.getText());
+      fail();
+    } catch (NoSuchElementException ex) {
+
+    }
   }
 }
