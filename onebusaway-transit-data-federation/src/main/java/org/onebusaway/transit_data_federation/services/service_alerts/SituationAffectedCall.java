a1589477f7dba815e95eb1dfa2ba950970dca761
jeffmaki <jmaki@openplans.org>
2011-09-20 18:59:23 -0400

    Merging current version of oba-app-mods/trunk

--- onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/services/service_alerts/SituationAffectedCall.java
+++
@@ -1,6 +0,0 @@
-package org.onebusaway.transit_data_federation.services.service_alerts;
-
-public class SituationAffectedCall extends SituationAffectedStop {
-
-  private static final long serialVersionUID = 1L;
-}
