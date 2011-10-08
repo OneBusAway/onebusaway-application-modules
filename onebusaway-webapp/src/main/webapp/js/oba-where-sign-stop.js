26cf22bdd32584293ebf86d611f6138d30db555b
Brian Ferris <bdferris@google.com>
2011-10-08 20:52:53 +0000

    Issue 501: Multiple stops in Sign mode overlap with transit text
    1) Remove fixed height of header.

--- onebusaway-webapp/src/main/webapp/js/oba-where-sign-stop.js
+++ onebusaway-webapp/src/main/webapp/js/oba-where-sign-stop.js
@@ -15,47 +15,51 @@
  */
 var oba_where_sign_stop = function() {
 	
-	var heightToScrollTime = function(height) {
+	var getScrollToBottomTimeForHeight = function(height) {
 		return height / 200 * 1000;
 	};
-	
+
+	var getScrollToTopTimeForHeight = function(height) {
+		return height / 2000 * 1000;
+	};
+
 	var registerScrollToBottom = function() {
 		
 		var body = jQuery('body');
 		var scroll = body.height() - jQuery(window).height();
-		var time = 29 * 1000 - heightToScrollTime(scroll);
+		var time = 29 * 1000 - getScrollToBottomTimeForHeight(scroll);
 		
 		window.setTimeout(scrollToBottom, 20*1000);
 	};
-	
+
 	var scrollToBottom = function() {
 		
 		var body = jQuery('body');
 		var scrollTop = body.scrollTop();
 		
 		var top = body.height() - jQuery(window).height();
-		var time = heightToScrollTime(top - scrollTop);
+		var time = getScrollToBottomTimeForHeight(top - scrollTop);
 		
 		jQuery('html, body').stop().animate({
 			scrollTop: top 
 		}, time, 'linear', registerScrollToTop);
 	};
-	
+
 	var registerScrollToTop = function() {
 		window.setTimeout(scrollToTop, 1*1000);
 	};
-	
+
 	var scrollToTop = function() {
 		
 		var root = jQuery('body'); 
 		var scrollTop = root.scrollTop();
 		
-		var time = heightToScrollTime(scrollTop);
+		var time = getScrollToTopTimeForHeight(scrollTop);
 		
 		jQuery('html, body').stop().animate({
 			scrollTop: 0 
 		}, time, 'linear', registerScrollToBottom);
 	};
-	
+
 	registerScrollToBottom();
 };
