6b074983f94274fcd6694793e07d4dba048b1e86
sheldonabrown <sheldonb@gmail.com>
2014-03-24 11:04:20 -0400

    RG-264, RG-265, RG-266 TDS, GTFS realtime support in watchdog, first pass at exposing unmatched trip ids and some other sample metrics.  Need to standardize on web service format
    Former-commit-id: ba1e10a1db62c83e378b3281404f3e8de1511907

---
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/MonitoredResult.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2011 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.onebusaway.gtfs.model.AgencyAndId;
+
+public class MonitoredResult {
+
+  private List<String> _agencyIds = new ArrayList<String>();
+  private Set<String> _unknownTripIds = new HashSet<String>();
+  private Set<AgencyAndId> _invalidBlockIds = new HashSet<AgencyAndId>();
+  private int _recordsTotal = 0;
+  private long _lastUpdate = System.currentTimeMillis();
+  
+  public void addUnknownTripId(String tripId) {
+    _unknownTripIds.add(tripId);
+  }
+
+  public Set<String> getUnknownTripIds() {
+    return _unknownTripIds;
+  }
+
+  
+  public void addInvalidBlockId(AgencyAndId id) {
+    _invalidBlockIds.add(id);
+  }
+
+  public Set<AgencyAndId> getInvalidBlockIds() {
+    return _invalidBlockIds;
+  }
+  
+  void setRecordsTotal(int size) {
+    _recordsTotal = size;
+  }
+  
+  public int getRecordsTotal() {
+    return _recordsTotal;
+  }
+
+  public void addRecordTotal() {
+    _recordsTotal = _recordsTotal + 1;
+    
+  }
+
+  public void addAgencyId(String agencyId) {
+    _agencyIds.add(agencyId);
+  }
+  
+  public List<String> getAgencyIds() {
+    return _agencyIds;
+  }
+
+  public void setAgencyIds(List<String> agencyIds) {
+    _agencyIds = agencyIds;
+  }
+  
+  public long getLastUpdate() {
+    return _lastUpdate;
+  }
+
+
+}
--- onebusaway-watchdog-webapp/src/main/java/org/onebusaway/watchdog/api/MetricResource.java
+++ onebusaway-watchdog-webapp/src/main/java/org/onebusaway/watchdog/api/MetricResource.java
@@ -15,26 +15,37 @@
  */
 package org.onebusaway.watchdog.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 import org.onebusaway.transit_data.services.TransitDataService;
+import org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime.MonitoredDataSource;
+import org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime.MonitoredResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Path("/metric")
-@Component
 public class MetricResource {
 
   private static Logger _log = LoggerFactory.getLogger(MetricResource.class);
   private TransitDataService _tds;
+  private List<MonitoredDataSource> _dataSources = null;
   
   @Autowired
   public void setTransitDataService(TransitDataService tds) {
     _tds = tds;
+  }
+  
+  @Autowired
+  public void setMonitoredDataSources(List<MonitoredDataSource> dataSources) {
+    _dataSources = dataSources;
   }
   
   @Path("/ping")
@@ -53,13 +64,120 @@ public class MetricResource {
   
   @Path("/agency-count")
   @GET
-  public Response countAgencies() {
+  public Response getAgencyCount() {
     try {
       int count = _tds.getAgenciesWithCoverage().size();
       return Response.ok("" + count).build();
     } catch (Exception e) {
-      _log.error("agency-count broke", e);
+      _log.error("getAgencyCount broke", e);
       return Response.serverError().build();
     }
   }
+  
+  @Path("/realtime/{agencyId}/total-records")
+  @GET
+  public Response getTotalRecords(@PathParam("agencyId") String agencyId) {
+    try {
+      int totalRecords = 0;
+      if (this._dataSources == null || this._dataSources.isEmpty()) {
+        _log.error("no configured data sources");
+        return Response.serverError().build();
+      }
+      
+      for (MonitoredDataSource mds : _dataSources) {
+        MonitoredResult result = mds.getMonitoredResult();
+        if (result == null) continue;
+        for (String mAgencyId : result.getAgencyIds()) {
+          if (agencyId.equals(mAgencyId)) {
+            totalRecords += result.getRecordsTotal();
+          }
+        }
+      }
+      return Response.ok("" + totalRecords).build();
+    } catch (Exception e) {
+      _log.error("getTotalRecords broke", e);
+      return Response.serverError().build();
+    }
+  }
+  
+  @Path("/realtime/{agencyId}/unmatched-trips")
+  @GET
+  public Response getUnmatchedTrips(@PathParam("agencyId") String agencyId) {
+    try {
+      int unmatchedTrips = 0;
+      if (this._dataSources == null || this._dataSources.isEmpty()) {
+        _log.error("no configured data sources");
+        return Response.serverError().build();
+      }
+      
+      for (MonitoredDataSource mds : _dataSources) {
+        MonitoredResult result = mds.getMonitoredResult();
+        if (result == null) continue;
+        for (String mAgencyId : result.getAgencyIds()) {
+          if (agencyId.equals(mAgencyId)) {
+            unmatchedTrips += result.getUnknownTripIds().size();
+          }
+        }
+      }
+      return Response.ok("" + unmatchedTrips).build();
+    } catch (Exception e) {
+      _log.error("getUnmatchedTrips broke", e);
+      return Response.serverError().build();
+    }
+  }
+
+  @Path("/realtime/{agencyId}/unmatched-trip-ids")
+  @GET
+  public Response getUnmatchedTripIds(@PathParam("agencyId") String agencyId) {
+    try {
+      List<String> unmatchedTripIds = new ArrayList<String>();
+      if (this._dataSources == null || this._dataSources.isEmpty()) {
+        _log.error("no configured data sources");
+        return Response.serverError().build();
+      }
+      
+      for (MonitoredDataSource mds : _dataSources) {
+        MonitoredResult result = mds.getMonitoredResult();
+        if (result == null) continue;
+        for (String mAgencyId : result.getAgencyIds()) {
+          if (agencyId.equals(mAgencyId)) {
+            unmatchedTripIds.addAll(result.getUnknownTripIds());
+          }
+        }
+      }
+      return Response.ok("" + unmatchedTripIds).build();
+    } catch (Exception e) {
+      _log.error("getUnmatchedTripIds broke", e);
+      return Response.serverError().build();
+    }
+  }
+
+  
+  @Path("/realtime/{agencyId}/last-update-delta")
+  @GET
+  public Response getLastUpdateDelta(@PathParam("agencyId") String agencyId) {
+    try {
+      long lastUpdate = 0;
+      if (this._dataSources == null || this._dataSources.isEmpty()) {
+        _log.error("no configured data sources");
+        return Response.serverError().build();
+      }
+      
+      for (MonitoredDataSource mds : _dataSources) {
+        MonitoredResult result = mds.getMonitoredResult();
+        if (result == null) continue;
+        for (String mAgencyId : result.getAgencyIds()) {
+          if (agencyId.equals(mAgencyId)) {
+            lastUpdate += result.getLastUpdate();
+          }
+        }
+      }
+      return Response.ok("" + (System.currentTimeMillis() - lastUpdate)/1000).build();
+    } catch (Exception e) {
+      _log.error("getLastUpdateDelta broke", e);
+      return Response.serverError().build();
+    }
+  }
+  
+  
 }
