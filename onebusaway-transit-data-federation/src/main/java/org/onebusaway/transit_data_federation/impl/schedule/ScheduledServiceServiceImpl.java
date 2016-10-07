b4fc3b5b2dc626d2b943fbcd62906937efc7ca94
sbrown <sbrown@192.168.207.70>
2016-10-07 07:41:41 -0700

    removing logging reference from last commit

--- onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/schedule/ScheduledServiceServiceImpl.java
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/impl/schedule/ScheduledServiceServiceImpl.java
@@ -17,7 +17,6 @@ package org.onebusaway.transit_data_federation.impl.schedule;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.onebusaway.gtfs.model.AgencyAndId;
 import org.onebusaway.transit_data.model.AgencyBean;
 import org.onebusaway.transit_data.model.StopBean;
@@ -190,7 +189,6 @@ public class ScheduledServiceServiceImpl implements ScheduleHelperService {
   @Override
   public List<String> getSearchSuggestions(String agencyId, String input) {
     // TODO Auto-generated method stub
-    _log.error("unimplemented getSearchSuggestions");
     return null;
   }
 
@@ -198,7 +196,6 @@ public class ScheduledServiceServiceImpl implements ScheduleHelperService {
   public List<StopBean> filterRevenueService(AgencyBean agency,
       StopsBean stops) {
     // TODO Auto-generated method stub
-    _log.error("unimplemented filterRevenueService");
     return stops.getStops();
   }
   
