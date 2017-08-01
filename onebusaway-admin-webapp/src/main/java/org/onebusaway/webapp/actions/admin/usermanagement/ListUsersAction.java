25946411206d45bbad3f99a30bba2fb801a189d1
sheldonabrown <sheldonb@gmail.com>
2017-08-01 15:57:27 -0400

    Remove list of just names

---
+++ onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/usermanagement/ListUsersAction.java
@@ -0,0 +1,36 @@
+package org.onebusaway.webapp.actions.admin.usermanagement;
+
+import org.onebusaway.admin.model.ui.UserDetail;
+import org.onebusaway.admin.service.UserManagementService;
+import org.onebusaway.webapp.actions.OneBusAwayNYCAdminActionSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Lists the users in the database.
+ *
+ */
+public class ListUsersAction extends OneBusAwayNYCAdminActionSupport {
+
+    private static Logger log = LoggerFactory.getLogger(ListUsersAction.class);
+    private UserManagementService userManagementService;
+
+    public void init() {
+
+    }
+
+    /**
+     * @param userManagementService the userManagementService to set
+     */
+    @Autowired
+    public void setUserManagementService(UserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
+    }
+
+    public List<UserDetail> getAllUserDetailsList() {
+        return userManagementService.getAllUserDetails();
+    }
+}
