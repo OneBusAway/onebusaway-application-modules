991e02fb2193454881b1011abf5fadc703113d1e
Heidebritta Frank <heidebritta@gmail.com>
2017-08-28 17:57:28 -0400

    Add initial pages for list API keys

---
+++ onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/apikeymanagement/ListApiKeysAction.java
@@ -0,0 +1,11 @@
+package org.onebusaway.webapp.actions.admin.apikeymanagement;
+
+import org.onebusaway.webapp.actions.OneBusAwayNYCAdminActionSupport;
+
+import java.util.List;
+
+public class ListApiKeysAction extends OneBusAwayNYCAdminActionSupport {
+
+
+   // public List<UserDetail> getUserDetailsList() { return userDetailsList; }
+}
