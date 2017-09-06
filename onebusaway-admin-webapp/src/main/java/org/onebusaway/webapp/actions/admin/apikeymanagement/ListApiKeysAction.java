c3268c21c234b3ad5f3cd035ba99791c1e7d472d
Heidebritta Frank <heidebritta@gmail.com>
2017-09-06 13:24:36 -0400

    List keys, add logic for retrieving/populating beans, fix UI table

--- onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/apikeymanagement/ListApiKeysAction.java
+++ onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/apikeymanagement/ListApiKeysAction.java
@@ -1,18 +1,71 @@
 package org.onebusaway.webapp.actions.admin.apikeymanagement;
 
+import org.onebusaway.users.client.model.UserBean;
+import org.onebusaway.users.model.User;
+import org.onebusaway.users.model.UserIndexKey;
+import org.onebusaway.users.model.UserIndex;
 import org.onebusaway.users.services.UserIndexTypes;
 import org.onebusaway.users.services.UserPropertiesService;
 import org.onebusaway.users.services.UserService;
 import org.onebusaway.webapp.actions.OneBusAwayNYCAdminActionSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListApiKeysAction extends OneBusAwayNYCAdminActionSupport {
 
+    private static Logger log = LoggerFactory.getLogger(ListApiKeysAction.class);
     private List<String> apiKeysList;
+    private List<UserBean> apiKeysUserBeansList = new ArrayList<UserBean>();
     private UserService userService;
     private UserPropertiesService userPropertiesService;
+
+    public String execute() {
+        super.execute();
+
+        generateUserBeans();
+        return SUCCESS;
+    }
+
+    public void generateUserBeans() {
+
+        for(String key : getApiKeysList()) {
+            UserIndexKey userKey = new UserIndexKey(UserIndexTypes.API_KEY, key);
+            UserIndex userIndexForId = userService.getUserIndexForId(userKey);
+            if (userIndexForId != null) {
+                User user = userIndexForId.getUser();
+                UserBean bean = userService.getUserAsBean(user);
+                if (bean != null) {
+                    getApiKeysUserBeansList().add(bean);
+                    log.error("** ! User bean id: " + bean.getUserId());
+                    log.error("** ! User bean index: " + bean.getIndices().get(0).getValue());
+                }
+            }
+        }
+
+        /*
+        // Check if key already exists
+    UserIndexKey userKey = new UserIndexKey(UserIndexTypes.API_KEY, key);
+    UserIndex userIndexForId = userService.getUserIndexForId(userKey);
+    if (userIndexForId == null) {
+      addActionMessage("Key '" + key + "' does not exist");
+    } else {
+      User user = userIndexForId.getUser();
+      UserBean bean = userService.getUserAsBean(user);
+      minApiReqInt = bean.getMinApiRequestInterval();
+      contactName = bean.getContactName();
+      contactCompany = bean.getContactCompany();
+      contactEmail = bean.getContactEmail();
+      contactDetails = bean.getContactDetails();
+      addActionMessage("Key '" + key + "' found");
+    }
+    return SUCCESS;
+  }
+         */
+    }
 
 
     /**
@@ -37,10 +90,16 @@ public class ListApiKeysAction extends OneBusAwayNYCAdminActionSupport {
         this.apiKeysList = apiKeysList;
     }
 
-    // public List<UserDetail> getUserDetailsList() { return userDetailsList; }
     public List<String> getApiKeysList() {
         return userService.getUserIndexKeyValuesForKeyType(UserIndexTypes.API_KEY);
     }
 
 
+    public List<UserBean> getApiKeysUserBeansList() {
+        return apiKeysUserBeansList;
+    }
+
+    public void setApiKeysUserBeansList(List<UserBean> apiKeysUserBeansList) {
+        this.apiKeysUserBeansList = apiKeysUserBeansList;
+    }
 }
