4bc6a2131e56d3d4a31d05b48b316f58a9c54c1b
sheldonabrown <sheldonb@gmail.com>
2017-10-24 11:06:59 -0400

    Fixes to have admin map render

--- onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/vehiclestatusmap/VehicleStatusMapAction.java
+++ onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/vehiclestatusmap/VehicleStatusMapAction.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2017 Cambridge Systematics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.onebusaway.webapp.actions.admin.vehiclestatusmap;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -5,6 +20,7 @@ import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.ActionProxy;
 import org.onebusaway.presentation.services.realtime.RealtimeService;
 import org.onebusaway.transit_data.model.service_alerts.ServiceAlertBean;
+import org.onebusaway.util.SystemTime;
 import org.onebusaway.util.services.configuration.ConfigurationService;
 import org.onebusaway.webapp.actions.OneBusAwayNYCAdminActionSupport;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +47,16 @@ public class VehicleStatusMapAction extends OneBusAwayNYCAdminActionSupport {
 
     public String getGoogleAdClientId() {
         return _configurationService.getConfigurationValueAsString("display.googleAdsClientId", "");
+    }
+
+    public String getAppHostName() {
+        return _configurationService.getConfigurationValueAsString(
+                "appHostname", null);
+
+    }
+
+    public String getCacheBreaker() {
+        return String.valueOf(SystemTime.currentTimeMillis());
     }
 
     public List<ServiceAlertBean> getGlobalServiceAlerts() {
