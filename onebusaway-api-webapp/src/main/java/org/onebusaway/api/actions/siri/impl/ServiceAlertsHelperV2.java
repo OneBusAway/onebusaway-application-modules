a5addf2d197ec4998bffe28209d08217b4c181ac
sheldonabrown <sheldonb@gmail.com>
2018-02-13 09:40:25 -0500

    Adding beta support for stop level service alerts in SIRI

--- onebusaway-api-webapp/src/main/java/org/onebusaway/api/actions/siri/impl/ServiceAlertsHelperV2.java
+++ onebusaway-api-webapp/src/main/java/org/onebusaway/api/actions/siri/impl/ServiceAlertsHelperV2.java
@@ -41,6 +41,7 @@ import org.onebusaway.transit_data.model.service_alerts.SituationQueryBean;
 import org.onebusaway.transit_data.services.TransitDataService;
 
 import uk.org.siri.siri_2.AffectedOperatorStructure;
+import uk.org.siri.siri_2.AffectedStopPointStructure;
 import uk.org.siri.siri_2.AffectedVehicleJourneyStructure;
 import uk.org.siri.siri_2.AffectsScopeStructure;
 import uk.org.siri.siri_2.AffectsScopeStructure.Operators;
@@ -61,6 +62,7 @@ import uk.org.siri.siri_2.SeverityEnumeration;
 import uk.org.siri.siri_2.SituationExchangeDeliveryStructure;
 import uk.org.siri.siri_2.SituationExchangeDeliveryStructure.Situations;
 import uk.org.siri.siri_2.SituationRefStructure;
+import uk.org.siri.siri_2.StopPointRefStructure;
 import uk.org.siri.siri_2.VehicleActivityStructure;
 
 public class ServiceAlertsHelperV2 {
@@ -405,6 +407,7 @@ public class ServiceAlertsHelperV2 {
       }
       String routeId = affects.getRouteId(); // LineRef
       String directionId = affects.getDirectionId();
+      String stopId = affects.getStopId();
       if (!StringUtils.isBlank(routeId)) {
         AffectedVehicleJourneyStructure affectedVehicleJourneyStructure = new AffectedVehicleJourneyStructure();
         LineRefStructure lineRefStructure = new LineRefStructure();
@@ -418,6 +421,18 @@ public class ServiceAlertsHelperV2 {
         vehicleJourneys.getAffectedVehicleJourney().add(
             affectedVehicleJourneyStructure);
       }
+      // add support for stop level alerts
+      if (!StringUtils.isBlank(stopId)) {
+        AffectedStopPointStructure stop = new AffectedStopPointStructure();
+        StopPointRefStructure stopRef = new StopPointRefStructure();
+        stopRef.setValue(stopId);
+        stop.setStopPointRef(stopRef);
+        if (affectsStructure.getStopPoints() == null) {
+          affectsStructure.setStopPoints(new AffectsScopeStructure.StopPoints());
+        }
+        affectsStructure.getStopPoints().getAffectedStopPoint().add(stop);
+      }
+
     }
     if (vehicleJourneys.getAffectedVehicleJourney().size() > 0) {
       affectsStructure.setVehicleJourneys(vehicleJourneys);
