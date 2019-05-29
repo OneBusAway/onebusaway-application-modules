bc93d4628f347cced922120919e1466050cd939d
carabalb <lennycaraballo@gmail.com>
2019-05-29 16:03:29 -0400

    Minor changes to vehicle assignments clear button
    - Added column header to clear button
    - Centered clear button
    - Remove checkmark if clear button is pressed
    - Add pointer cursor when hovering over button

--- onebusaway-admin-webapp/src/main/webapp/js/oba/vehicleblockassignment/vehicle-block-assignment.js
+++ onebusaway-admin-webapp/src/main/webapp/js/oba/vehicleblockassignment/vehicle-block-assignment.js
@@ -139,7 +139,9 @@ jQuery(function() {
 
     $(".close").click(function(e){
         e.preventDefault();
+        $(this).parent().parent().find(".checkmark").hide();
         $(this).parent().parent().find(".custom-combobox-input").val("");
+
 
     })
 });
