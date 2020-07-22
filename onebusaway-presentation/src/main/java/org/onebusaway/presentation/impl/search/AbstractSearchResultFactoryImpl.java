2f26fdc03d075cd77eae5816f06f31ea6d2b3b97
Rebecca Brown <rbrown@camsys.com>
2020-07-22 14:21:03 -0400

    HART-115 Added link support for service alerts on mobile site

--- onebusaway-presentation/src/main/java/org/onebusaway/presentation/impl/search/AbstractSearchResultFactoryImpl.java
+++ onebusaway-presentation/src/main/java/org/onebusaway/presentation/impl/search/AbstractSearchResultFactoryImpl.java
@@ -42,9 +42,9 @@ public abstract class AbstractSearchResultFactoryImpl implements SearchResultFac
       // look for both summary and descriptions; if both trivially append for display
       descriptionsAdded = setDescription(serviceAlertDescriptions,
               serviceAlertBean.getSummaries(),
-              serviceAlertBean.getDescriptions(), htmlizeNewlines)
+              serviceAlertBean.getDescriptions(), serviceAlertBean.getUrls(), htmlizeNewlines)
           || setDescription(serviceAlertDescriptions,
-              serviceAlertBean.getSummaries(), htmlizeNewlines);
+              serviceAlertBean.getSummaries(), serviceAlertBean.getUrls(), htmlizeNewlines);
       if (!descriptionsAdded) {
         serviceAlertDescriptions.add("(no description)");
       }
@@ -66,14 +66,24 @@ public abstract class AbstractSearchResultFactoryImpl implements SearchResultFac
     populateServiceAlerts(serviceAlertDescriptions, serviceAlertBeans, true);
   }
 
-  private boolean setDescription(Set<String> serviceAlertDescriptions, List<NaturalLanguageStringBean> descriptions, boolean htmlizeNewlines) {
+  private boolean setDescription(Set<String> serviceAlertDescriptions, List<NaturalLanguageStringBean> descriptions, List<NaturalLanguageStringBean> urls, boolean htmlizeNewlines) {
     boolean descriptionsAdded = false;
     if (descriptions != null) {
-      for (NaturalLanguageStringBean description : descriptions) {
-        if (description.getValue() != null) {
+      for (int i = 0; i < descriptions.size(); i++) {
+        NaturalLanguageStringBean description = descriptions.get(i);
+        if (description.getValue() != null && !description.getValue().isEmpty()) {
           StringBuffer text = new StringBuffer();
-          if (htmlizeNewlines)
+          if (htmlizeNewlines) {
+            if (urls != null && urls.size() > i) {
+              text.append("<a href='");
+              text.append(urls.get(i).getValue());
+              text.append("' target='_blank'>");
+            }
             text.append(description.getValue().replace("\n", "<br/>"));
+            if (urls != null && urls.size() > i) {
+              text.append("</a>");
+            }
+          }
           else
             text.append(description.getValue());
           serviceAlertDescriptions.add(text.toString());
@@ -84,7 +94,7 @@ public abstract class AbstractSearchResultFactoryImpl implements SearchResultFac
     return descriptionsAdded;
   }
 
-  private boolean setDescription(Set<String> serviceAlertDescriptions, List<NaturalLanguageStringBean> summaries, List<NaturalLanguageStringBean> descriptions, boolean htmlizeNewlines) {
+  private boolean setDescription(Set<String> serviceAlertDescriptions, List<NaturalLanguageStringBean> summaries, List<NaturalLanguageStringBean> descriptions, List<NaturalLanguageStringBean> urls, boolean htmlizeNewlines) {
     boolean descriptionsAdded = false;
 
     if (summaries != null) {
@@ -96,16 +106,26 @@ public abstract class AbstractSearchResultFactoryImpl implements SearchResultFac
           NaturalLanguageStringBean summary = summaries.get(0);
           NaturalLanguageStringBean description = descriptions.get(0);
 
-          if (summary.getValue() != null) {
+          if (urls != null && urls.size() > i) {
+            text.append("<a href='");
+            text.append(urls.get(i).getValue());
+            text.append("' target='_blank'>");
+          }
+
+          if (summary.getValue() != null && !summary.getValue().isEmpty()) {
             foundSummary = true;
             text.append("<strong>");
             if (htmlizeNewlines) text.append(summary.getValue());
           }
-          if (description.getValue() != null) {
+          if (description.getValue() != null && !description.getValue().isEmpty()) {
             if (foundSummary && htmlizeNewlines) text.append("</strong><br/><br/>");
             text.append(description.getValue());
           } else {
             if (foundSummary && htmlizeNewlines) text.append("</strong>");
+          }
+
+          if (urls != null && urls.size() > i) {
+            text.append("</a>");
           }
 
           serviceAlertDescriptions.add(htmlizeNewlines ? text.toString().replace("\n",
@@ -117,19 +137,49 @@ public abstract class AbstractSearchResultFactoryImpl implements SearchResultFac
       if (descriptionsAdded) return descriptionsAdded;
 
       // if we are here our summaries and description don't match
-      for (NaturalLanguageStringBean summary : summaries) {
-        if (summary.getValue() != null) {
+      for (int i = 0; i < summaries.size(); i++) {
+        NaturalLanguageStringBean summary = summaries.get(i);
+        if (summary.getValue() != null && !summary.getValue().isEmpty()) {
+          StringBuilder text = new StringBuilder();
+
+          if (urls != null && urls.size() > i) {
+            text.append("<a href='");
+            text.append(urls.get(i).getValue());
+            text.append("' target='_blank'>");
+          }
+
+          text.append(summary.getValue());
+
+          if (urls != null && urls.size() > i) {
+            text.append("</a>");
+          }
+
           serviceAlertDescriptions.add(htmlizeNewlines ? "<strong>" + summary.getValue().replace("\n",
-                  "<br/>") + "</strong>" : summary.getValue());
+                  "<br/>") + "</strong>" : text.toString());
           descriptionsAdded = true;
         }
       }
     }
     if (descriptions != null) {
-      for (NaturalLanguageStringBean description : descriptions) {
-        if (description.getValue() != null) {
+      for (int i = 0; i < descriptions.size(); i++) {
+        NaturalLanguageStringBean description = descriptions.get(i);
+        if (description.getValue() != null && !description.getValue().isEmpty()) {
+          StringBuilder text = new StringBuilder();
+
+          if (urls != null && urls.size() > i) {
+            text.append("<a href='");
+            text.append(urls.get(i).getValue());
+            text.append("' target='_blank'>");
+          }
+
+          text.append(description.getValue());
+
+          if (urls != null && urls.size() > i) {
+            text.append("</a>");
+          }
+
           serviceAlertDescriptions.add((htmlizeNewlines ? description.getValue().replace("\n",
-                  "<br/>") : description.getValue()));
+                  "<br/>") : text.toString()));
           descriptionsAdded = true;
         }
       }
