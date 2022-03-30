398820aedb635c36e49c715f6f626d9905c66424
sheldonabrown <sheldonb@gmail.com>
2022-03-30 09:22:26 -0400

    OBAHART-146 more allowed DMI annotations

--- onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/usermanagement/ListUsersAction.java
+++ onebusaway-admin-webapp/src/main/java/org/onebusaway/webapp/actions/admin/usermanagement/ListUsersAction.java
@@ -15,6 +15,7 @@
  */
 package org.onebusaway.webapp.actions.admin.usermanagement;
 
+import org.apache.struts2.convention.annotation.AllowedMethods;
 import org.onebusaway.admin.model.ui.UserDetail;
 import org.onebusaway.admin.service.UserManagementService;
 import org.onebusaway.webapp.actions.OneBusAwayNYCAdminActionSupport;
@@ -28,6 +29,7 @@ import java.util.List;
  * Lists the users in the database.
  *
  */
+@AllowedMethods({"firstPage", "nextPage", "previousPage"})
 public class ListUsersAction extends OneBusAwayNYCAdminActionSupport {
 
     private static Logger log = LoggerFactory.getLogger(ListUsersAction.class);
