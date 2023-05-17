5ae113d0efd21a1982df09abe5bca3decc0b785e
sheldonabrown <sheldonb@gmail.com>
2023-05-17 08:41:46 -0400

    missed files from last commit, and shell of duplicate int test

--- onebusaway-gtfsrt-integration-tests/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/STDuplicatedTripsIntegrationTest.java
+++ onebusaway-gtfsrt-integration-tests/src/test/java/org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/STDuplicatedTripsIntegrationTest.java
@@ -23,37 +23,46 @@ import org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime.GtfsRe
 import org.onebusaway.transit_data_federation.impl.realtime.gtfs_realtime.MonitoredResult;
 import org.springframework.core.io.ClassPathResource;
 
-import static org.junit.Assert.assertFalse;
-
-public class NyctAddedTripsIntegrationTest extends AbstractGtfsRealtimeIntegrationTest {
-
+public class STDuplicatedTripsIntegrationTest extends AbstractGtfsRealtimeIntegrationTest  {
   protected String getIntegrationTestPath() {
-    return "org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/nyct_added_trips";
+    return "org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/st_duplicated_trips";
   }
 
   protected String[] getPaths() {
     String[] paths = {"test-data-sources.xml"};
     return paths;
   }
+
   @Test
-  public void testAddedViaExtension() throws Exception {
+  public void testDuplicatedTrips1() throws Exception {
     GtfsRealtimeSource source = getBundleLoader().getSource();
-    source.setAgencyId("MTASBWY");
+    source.setAgencyId("ST");
 
     VehicleLocationListener listener = new TestVehicleLocationListener();
     source.setVehicleLocationListener(listener);
     MonitoredResult testResult = new MonitoredResult();
     source.setMonitoredResult(testResult);
 
-    // this is the gtfs-rt protocol-buffer file to match to the loaded bundle
-    String gtfsrtFilename = "org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/nyct_added_trips/nyct_subways_gtfs_rt.pb";
+    // todo examples so far in JSON -- check to see if we can get PB or if we need to convert
+    String gtfsrtFilename = "org/onebusaway/transit_data_federation/impl/realtime/gtfs_realtime/integration_tests/st_duplicated_trips/trip_update_1683740698.json";
     ClassPathResource gtfsRtResource = new ClassPathResource(gtfsrtFilename);
     if (!gtfsRtResource.exists()) throw new RuntimeException(gtfsrtFilename + " not found in classpath!");
     source.setTripUpdatesUrl(gtfsRtResource.getURL());
     source.refresh(); // launch
+    // this is an example provided in the referenced JSON
+/*
+      "id": "1BB8C2B3-D3DA-4C8B-ABCC-3C042CA40C18",
+      "trip_update": {
+        "trip": {
+          "trip_id": "LLR_2023-03-18_Weekday_100479_1043",
+          "route_id": "100479",
+          "direction_id": 1,
+          "start_time": "10:43:00",
+          "start_date": "20230510",
+          "schedule_relationship": "DUPLICATED"
+        },
 
-    // check TestVehicleLocationListenr for expected service
-    // check MonitoredResult for expected number of results
-    assertFalse(testResult.getAddedTripIds().isEmpty()); // this doesn't work yet!
+ */
+    // todo do we want to differentiate added from duplicated?
   }
 }
