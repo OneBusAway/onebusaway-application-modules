c8465514af4dbfe3a0bb6270923a01301be35068
Merhatsidk Ayele <merhatsidk29@gmail.com>
2023-05-18 11:26:31 -0400

    created dynamic classes for ADDED trips.

---
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/services/transit_graph/dynamic/DynamicStopTimeEntryImpl.java
@@ -0,0 +1,192 @@
+/**
+ * Copyright (C) 2013 Kurt Raschke <kurt@kurtraschke.com>
+ * Copyright (C) 2011 Brian Ferris <bdferris@onebusaway.org>
+ * Copyright (C) 2011 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.transit_data_federation.services.transit_graph.dynamic;
+
+import org.onebusaway.realtime.api.OccupancyStatus;
+import org.onebusaway.transit_data_federation.services.transit_graph.StopTimeEntry;
+
+import java.io.Serializable;
+
+public class DynamicStopTimeEntryImpl implements StopTimeEntry, Serializable {
+    private static final long serialVersionUID = 9L;
+
+    private int _stopTimeId;
+    private int _arrivalTime;
+    private int _departureTime;
+    private OccupancyStatus _historicalOccupancy;
+    private int _sequence;
+    private int _gtfsSequence;
+    private int _dropOffType;
+    private int _pickupType;
+    private int _shapePointIndex = -1;
+    private double _shapeDistTraveled = Double.NaN;
+    private int _accumulatedSlackTime = 0;
+    private int _totalStopsInTrip;
+
+    private DynamicStopEntryImpl _stop;
+
+    private DynamicTripEntryImpl _trip;
+
+    public void setId(int id) {
+        _stopTimeId = id;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        _arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureTime(int departureTime) {
+        _departureTime = departureTime;
+    }
+
+    public void setHistoricalOccupancy(OccupancyStatus historicalOccupancy) {_historicalOccupancy = historicalOccupancy; }
+
+    public DynamicStopTimeEntryImpl setTime(int time) {
+        _arrivalTime = time;
+        _departureTime = time;
+        return this;
+    }
+
+    public void setSequence(int sequence) {
+        _sequence = sequence;
+    }
+
+    public void setGtfsSequence(int gtfsSequence) {
+        _gtfsSequence = gtfsSequence;
+    }
+
+    public void setDropOffType(int dropOffType) {
+        _dropOffType = dropOffType;
+    }
+
+    public void setPickupType(int pickupType) {
+        _pickupType = pickupType;
+    }
+
+    public DynamicStopTimeEntryImpl setStop(DynamicStopEntryImpl stop) {
+        _stop = stop;
+        return this;
+    }
+
+    public void setTrip(DynamicTripEntryImpl trip) {
+        _trip = trip;
+    }
+
+    public void setShapePointIndex(int shapePointIndex) {
+        _shapePointIndex = shapePointIndex;
+    }
+
+    public boolean isShapeDistTraveledSet() {
+        return !Double.isNaN(_shapeDistTraveled);
+    }
+
+    public void setShapeDistTraveled(double shapeDistTraveled) {
+        _shapeDistTraveled = shapeDistTraveled;
+    }
+
+    public void setAccumulatedSlackTime(int accumulatedSlackTime) {
+        _accumulatedSlackTime = accumulatedSlackTime;
+    }
+
+    public void setTotalStopsInTrip(int totalStopsInTrip) {
+        _totalStopsInTrip = totalStopsInTrip;
+    }
+
+    /****
+     * {@link StopTimeEntry} Interface
+     ****/
+
+    @Override
+    public int getId() {
+        return _stopTimeId;
+    }
+
+    @Override
+    public int getArrivalTime() {
+        return _arrivalTime;
+    }
+
+    @Override
+    public int getDepartureTime() {
+        return _departureTime;
+    }
+
+    @Override
+    public OccupancyStatus getHistoricalOccupancy() { return _historicalOccupancy; }
+
+    @Override
+    public int getSequence() {
+        return _sequence;
+    }
+
+    @Override
+    public int getGtfsSequence() {
+        return _gtfsSequence;
+    }
+
+    @Override
+    public int getDropOffType() {
+        return _dropOffType;
+    }
+
+    @Override
+    public int getPickupType() {
+        return _pickupType;
+    }
+
+    @Override
+    public DynamicStopEntryImpl getStop() {
+        return _stop;
+    }
+
+    @Override
+    public DynamicTripEntryImpl getTrip() {
+        return _trip;
+    }
+
+    @Override
+    public int getShapePointIndex() {
+        return _shapePointIndex;
+    }
+
+    @Override
+    public double getShapeDistTraveled() {
+        return _shapeDistTraveled;
+    }
+
+    @Override
+    public int getSlackTime() {
+        return _departureTime - _arrivalTime;
+    }
+
+    @Override
+    public int getAccumulatedSlackTime() {
+        return _accumulatedSlackTime;
+    }
+
+    @Override
+    public int getTotalStopsInTrip() {
+        return _totalStopsInTrip;
+    }
+
+    @Override
+    public String toString() {
+        return "DynamicStopTimeEntryImpl(stop=" + _stop.getId() + " trip=" + _trip
+                + " arrival=" + _arrivalTime + " departure=" + _departureTime + ")";
+    }
+}
---
+++ onebusaway-transit-data-federation/src/main/java/org/onebusaway/transit_data_federation/services/transit_graph/dynamic/DynamicTripEntryImpl.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright (C) 2011 Brian Ferris <bdferris@onebusaway.org>
+ * Copyright (C) 2011 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.transit_data_federation.services.transit_graph.dynamic;
+
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.gtfs.model.calendar.LocalizedServiceId;
+import org.onebusaway.transit_data_federation.impl.transit_graph.TransitGraphImpl;
+import org.onebusaway.transit_data_federation.services.transit_graph.*;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.List;
+
+public class DynamicTripEntryImpl implements TripEntry, Serializable {
+
+    private static final long serialVersionUID = 7L;
+
+    private AgencyAndId _id;
+
+    private DynamicRouteEntry _route;
+
+    private String _directionId;
+
+    private DynamicBlockEntry _block;
+
+    private LocalizedServiceId _serviceId;
+
+    private AgencyAndId _shapeId;
+
+    private List<StopTimeEntry> _stopTimes;
+
+    private double _totalTripDistance;
+
+    private FrequencyEntry _frequencyLabel;
+
+    public DynamicTripEntryImpl setId(AgencyAndId id) {
+        _id = id;
+        return this;
+    }
+
+    public DynamicTripEntryImpl setRoute(DynamicRouteEntry route) {
+        _route = route;
+        return this;
+    }
+
+    public DynamicTripEntryImpl setDirectionId(String directionId) {
+        _directionId = directionId;
+        return this;
+    }
+
+    public DynamicTripEntryImpl setBlock(DynamicBlockEntry block) {
+        _block = block;
+        return this;
+    }
+
+    public DynamicTripEntryImpl setServiceId(LocalizedServiceId serviceId) {
+        _serviceId = serviceId;
+        return this;
+    }
+
+    public void setShapeId(AgencyAndId shapeId) {
+        _shapeId = shapeId;
+    }
+
+    public void setStopTimes(List<StopTimeEntry> stopTimes) {
+        _stopTimes = stopTimes;
+    }
+
+    public void setTotalTripDistance(double totalTripDistance) {
+        _totalTripDistance = totalTripDistance;
+    }
+
+    public void setFrequencyLabel(FrequencyEntry frequencyLabel) {
+        _frequencyLabel = frequencyLabel;
+    }
+
+    /****
+     * {@link TripEntry} Interface
+     ****/
+
+    @Override
+    public AgencyAndId getId() {
+        return _id;
+    }
+
+    @Override
+    public RouteEntry getRoute() {
+        return _route;
+    }
+
+    @Override
+    public RouteCollectionEntry getRouteCollection() {
+        return _route.getParent();
+    }
+
+    @Override
+    public String getDirectionId() {
+        return _directionId;
+    }
+
+    @Override
+    public DynamicBlockEntry getBlock() {
+        return _block;
+    }
+
+    @Override
+    public LocalizedServiceId getServiceId() {
+        return _serviceId;
+    }
+
+    @Override
+    public AgencyAndId getShapeId() {
+        return _shapeId;
+    }
+
+    @Override
+    public List<StopTimeEntry> getStopTimes() {
+        return _stopTimes;
+    }
+
+    @Override
+    public double getTotalTripDistance() {
+        return _totalTripDistance;
+    }
+
+    @Override
+    public FrequencyEntry getFrequencyLabel() {
+        return _frequencyLabel;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip(" + _id + ")";
+    }
+
+    /****
+     * Serialization
+     ****/
+
+    private void readObject(ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        in.defaultReadObject();
+        TransitGraphImpl.handleTripEntryRead(this);
+    }
+}
