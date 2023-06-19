96f451515c13bdacb6ba4f5875db9ec21c811a80
Merhatsidk Ayele <merhatsidk29@gmail.com>
2023-06-19 08:07:06 -0400

    Incremented GTFS-RT API to 2.0.

---
+++ onebusaway-api-core/src/main/java/org/onebusaway/api/model/transit/realtime/GtfsRealtimeConstantsV2.java
@@ -0,0 +1,9 @@
+package org.onebusaway.api.model.transit.realtime;
+
+public class GtfsRealtimeConstantsV2 {
+    public static final String VERSION = "2.0";
+
+    public GtfsRealtimeConstantsV2() {
+    }
+
+}
